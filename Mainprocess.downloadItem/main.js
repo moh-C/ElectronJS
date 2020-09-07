d160430d73723800c68288938b0696465f3931e9
Aaron <mohammad.chegini@hotmail.com>
2020-09-07 14:48:02 +0430

    downloadItem

---
+++ Mainprocess.downloadItem/main.js
@@ -0,0 +1,60 @@
+// Modules
+const { app, BrowserWindow, session } = require("electron");
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow() {
+  let ses = session.defaultSession;
+
+  mainWindow = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: { nodeIntegration: true },
+  });
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadFile("index.html");
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
+
+  ses.on("will-download", (e, downloadItem, webContents) => {
+    let fileName = downloadItem.getFilename();
+    let fileSize = downloadItem.getTotalBytes();
+    console.log(fileSize);
+
+    // Save to desktop
+    downloadItem.setSavePath(app.getPath("desktop") + `/${fileName}`);
+
+    downloadItem.on("updated", (e, state) => {
+      let received = downloadItem.getReceivedBytes();
+
+      if (state === "progressing" && received) {
+        let progress = Math.round((received / fileSize) * 100);
+        console.log(progress);
+        webContents.executeJavaScript(`window.progress.value = ${progress}`);
+      }
+    });
+  });
+
+  // Listen for window being closed
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+}
+
+// Electron `app` is ready
+app.on("ready", createWindow);
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on("activate", () => {
+  if (mainWindow === null) createWindow();
+});
