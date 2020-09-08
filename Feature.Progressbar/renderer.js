6581800f52b185bc37562efc19c952c59c89085b
Aaron <mohammad.chegini@hotmail.com>
2020-09-08 20:30:26 +0430

    Progressbar

---
+++ Feature.Progressbar/renderer.js
@@ -0,0 +1,13 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+
+const { ipcRenderer } = require("electron");
+
+ipcRenderer.on("progressbar-notification", (e, done) => {
+  if (done) {
+    new Notification("Done...", {
+      body: "The task is now complete",
+    });
+  }
+});
